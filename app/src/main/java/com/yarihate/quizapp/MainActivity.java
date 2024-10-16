273d3d8a2ce1bfc20c8e3d5d60cdd673646d714e
Valeriya Gelasimova <valeriyagelasimova@gmail.com>
2024-10-16 22:54:18 +0300

    ass test layout ScrollView

--- app/src/main/java/com/yarihate/quizapp/MainActivity.java
+++ app/src/main/java/com/yarihate/quizapp/MainActivity.java
@@ -1,14 +1,24 @@
 package com.yarihate.quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // загрузка интерфейса из файла activity_main.xml
+        setContentView(R.layout.scroll_view_test);
+
+
+//        setContentView(R.layout.second_layout);
+//        TextView textView = findViewById(R.id.header);
+//        // переустанавливаем у него текст
+//        textView.setText("Hello from Java!");
     }
 }
