11fa257c090bfd6ba6999e9058fbf76739bf99c6
Valeriya Gelasimova <valeriyagelasimova@gmail.com>
2024-11-04 21:49:28 +0300

    highlight right or wrong answers

--- app/src/main/java/com/yarihate/quizapp/activity/QuestionActivity.java
+++ app/src/main/java/com/yarihate/quizapp/activity/QuestionActivity.java
@@ -3,6 +3,7 @@ package com.yarihate.quizapp.activity;
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -42,6 +43,7 @@ public class QuestionActivity extends AppCompatActivity {
     private LinearLayout answersContainer;
     private String selectedAnswer;
     private int currentQuestionIndex = 0;
+    private Button selectedAnswewrButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,12 +54,9 @@ public class QuestionActivity extends AppCompatActivity {
         categoryId = getIntent().getIntExtra("category_id", -1);
         quizId = getIntent().getIntExtra("quiz_id", -1);
         if (categoryId == -1 || quizId == -1) {
-            //todo не знаю что тут делать
+            Toast.makeText(this, "Ошибка загрузки данных", Toast.LENGTH_SHORT).show();
+            finish();
             return;
-
-//            Toast.makeText(this, "Ошибка загрузки данных", Toast.LENGTH_SHORT).show();
-//            finish(); // Завершаем Activity, если данные не переданы
-//            return;
         }
 
         List<Category> categories = categoryService.getCategories();
@@ -77,42 +76,41 @@ public class QuestionActivity extends AppCompatActivity {
         answersContainer = findViewById(R.id.answers_container);
         Button submitButton = findViewById(R.id.submit_answer);
 
-        // Отображаем первый вопрос
         if (!questions.isEmpty()) {
             displayQuestion(questions.get(currentQuestionIndex));
         } else {
             Toast.makeText(this, "Вопросы не найдены", Toast.LENGTH_SHORT).show();
         }
 
-        // Обработчик для кнопки "Ответить"
         submitButton.setOnClickListener(v -> {
             if (selectedAnswer == null) {
                 Toast.makeText(this, "Выберите ответ", Toast.LENGTH_SHORT).show();
-            } else if (selectedAnswer.equals(questions.get(currentQuestionIndex).getCorrectAnswer())) {
-                rightAnswersCount++;
-                Toast.makeText(this, "Правильно!", Toast.LENGTH_SHORT).show();
-            } else {
-                Toast.makeText(this, "Неправильно!", Toast.LENGTH_SHORT).show();
+                return;
             }
 
-            // Переход к следующему вопросу
-            currentQuestionIndex++;
-            if (currentQuestionIndex < questions.size()) {
-                displayQuestion(questions.get(currentQuestionIndex));
+            if (selectedAnswer.equals(questions.get(currentQuestionIndex).getCorrectAnswer())) {
+                rightAnswersCount++;
+                selectedAnswewrButton.setBackgroundTintList(ColorStateList.valueOf(Color.GREEN));
             } else {
-                Toast.makeText(this, "Квиз завершён!", Toast.LENGTH_SHORT).show();
-                finish(); // Завершаем активити после последнего вопроса
+                selectedAnswewrButton.setBackgroundTintList(ColorStateList.valueOf(Color.RED));
             }
+
+            new Handler().postDelayed(() -> {
+                currentQuestionIndex++;
+                if (currentQuestionIndex < questions.size()) {
+                    displayQuestion(questions.get(currentQuestionIndex));
+                } else {
+                    Toast.makeText(this, "Квиз завершён!", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            }, 1000);
         });
     }
 
     private void displayQuestion(Question question) {
         questionText.setText(question.getQuestionText());
-
-        // Очищаем контейнер перед добавлением новых кнопок
         answersContainer.removeAllViews();
 
-        // Динамически создаём кнопки для каждого варианта ответа
         for (String answer : question.getAnswerOptions()) {
             Button answerButton = new Button(this);
             answerButton.setLayoutParams(new LinearLayout.LayoutParams(
@@ -123,7 +121,13 @@ public class QuestionActivity extends AppCompatActivity {
             answerButton.setBackgroundResource(R.drawable.answer_background);
             answerButton.setBackgroundTintList(getResources().getColorStateList(question.getBackgroundColor()));
             answerButton.setTextColor(question.getTitleColor());
+
+            LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) answerButton.getLayoutParams();
+            params.setMargins(0, 0, 0, 16);
+            answerButton.setLayoutParams(params);
+
             answerButton.setOnClickListener(v -> {
+                selectedAnswewrButton = answerButton;
                 selectedAnswer = answer;
                 highlightSelectedAnswer(answerButton, question.getBackgroundColor());
             });
@@ -133,17 +137,14 @@ public class QuestionActivity extends AppCompatActivity {
     }
 
     private void highlightSelectedAnswer(Button selectedButton, int questionBackGroundColour) {
-        // Проходим по всем кнопкам и сбрасываем им цвет на исходный
         for (int i = 0; i < answersContainer.getChildCount(); i++) {
             View child = answersContainer.getChildAt(i);
             if (child instanceof Button) {
                 Button button = (Button) child;
-                button.setBackgroundResource(R.drawable.answer_background); // Фон с закруглёнными углами
+                button.setBackgroundResource(R.drawable.answer_background);
                 button.setBackgroundTintList(getResources().getColorStateList(questionBackGroundColour));
             }
         }
-
-        // Устанавливаем цвет только выбранной кнопке
         selectedButton.setBackgroundTintList(ColorStateList.valueOf(Color.CYAN));
     }
 
