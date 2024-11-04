5af2c744c53ac8a9f9ab8abbd86c652b0229eedb
Valeriya Gelasimova <valeriyagelasimova@gmail.com>
2024-11-04 22:01:31 +0300

    fix necessary starts for pass quiz

--- app/src/main/java/com/yarihate/quizapp/dto/Constants.java
+++ app/src/main/java/com/yarihate/quizapp/dto/Constants.java
@@ -3,7 +3,7 @@ package com.yarihate.quizapp.dto;
 public class Constants {
     public static final String USER_STATE = "user_state";
     public static final String CATEGORY_STATISTIC = "category_statistic";
-    public static final int STARS_COUNT_FOR_COMPLETE_QUIZ = 3;
+    public static final int STARS_COUNT_FOR_COMPLETE_QUIZ = 1;
     public static final int MIN_PERCENT_FOR_1_STAR = 50;
     public static final int MIN_PERCENT_FOR_2_STARS = 75;
     public static final int MIN_PERCENT_FOR_3_STARS = 100;
